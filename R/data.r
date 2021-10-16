e0ce6b02f11148233d4e4a1620e52be5bd78ff49
xzj-14035 <xzj14035@outlook.com>
2021-10-16 13:08:56 +0800

    first commit

---
+++ R/data.r
@@ -0,0 +1,11 @@
+#' @title oneway_1
+#' @description 将27只3月龄雌性SD大鼠随机分为3组，每组9只。3组分别是空白对照组，去卵巢
+#' 组和雌激素组。90天后测量骨骼变化情况，用骨小梁面积百分比评价得到的结果
+#' @format
+#' \describe{
+#'      \item{group}{分组变量(空白对照组，去卵巢组，雌激素组)}
+#'      \item{tbar}{骨小梁面积百分比}
+#' }
+#' @docType data
+#' @name oneway_1
+NULL
