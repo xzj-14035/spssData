986adb6b7d262e4bffbdcc216f9a2bbc4325d622
xzj-14035 <xzj14035@outlook.com>
2021-10-18 00:52:45 +0800

    add oneway_2

--- R/data.r
+++ R/data.r
@@ -9,3 +9,18 @@
 #' @docType data
 #' @name oneway_1
 NULL
+
+#' @title oneway_2
+#' @description 将27只3月龄雌性SD大鼠随机分为3组，每组9只。3组分别是空白对照组，去卵巢
+#' 组和雌激素组, 90天后测量骨骼变化情况。
+#' @format 
+#' \describe{
+#'      \item{group}{分组变量（空白对照组，去卵巢组，雌激素组）}
+#'      \item{tbar}{骨小梁面积百分比}
+#'      \item{tbn}{骨小梁数目}
+#'      \item{tbsp}{骨小梁间隙}
+#'      \item{lpm}{荧光标记周长百分比}
+#'      \item{bfr}{骨形成率}
+#' @docType data
+#' @name oneway_2
+NULL 
